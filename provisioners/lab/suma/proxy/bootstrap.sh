0679050cbbe4765b76adaa5e54cf314d6e3c9c85
Samuel Snow <ssnow@suse.com>
2024-04-25 22:35:30 -0600

    Update bootstrap.sh

--- provisioners/lab/suma/proxy/bootstrap.sh
+++ provisioners/lab/suma/proxy/bootstrap.sh
@@ -5,14 +5,15 @@ sed -i 's/rpm.install.excludedocs = yes/rpm.install.excludedocs = no/' /etc/zypp
 sed -i "s/\$SUBNET/${SUBNET}/g" /tmp/hosts
 cp /tmp/hosts /etc/hosts
 
+SUSEConnect -r $SLEREGCODE
+zypper install -y --oldpackage suseconnect-ng-1.1.0~git2.f42b4b2a060e-150400.3.13.1
+SUSEConnect --de-register
+SUSEConnect --cleanup
 rpm -e --nodeps sles-release
-SUSEConnect -r $SUMAPROXYREGCODE -p SUSE-Manager-Proxy/4.2/x86_64
-SUSEConnect -p sle-module-basesystem/15.3/x86_64
-SUSEConnect -p sle-module-server-applications/15.3/x86_64
-SUSEConnect -p sle-module-suse-manager-proxy/4.2/x86_64
-
-## Depency issue with python-base and python-xml (temporary I think)
-# Technically could wait to patch until bootstrapped to SUMA, so not the worst thing.
+SUSEConnect -r $SUMAPROXYREGCODE -p SUSE-Manager-Proxy/4.3/x86_64
+SUSEConnect -p sle-module-basesystem/15.4/x86_64
+SUSEConnect -p sle-module-server-applications/15.4/x86_64
+SUSEConnect -p sle-module-suse-manager-proxy/4.3/x86_64
 
 zypper install -y man man-pages-posix man-pages rsyslog vim-data aaa_base-extras wget zypper-log
 systemctl enable --now rsyslog
